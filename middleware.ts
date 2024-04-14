4b323571d88fc5b14e3d29b2ca24302799d3af59
balldev1 <balldev1@hotmail.com>
2024-04-14 13:41:36 +0700

    2.37 Middleware & Login

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,48 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 
+import {
+    publicRoutes,
+    authRoutes,
+    apiAuthPrefix,
+    DEFAULT_LOGIN_REDIRECT
+} from "@/routes"
+
 const { auth } = NextAuth(authConfig);
 
 //ตรวจสอบสถานะ Login
-export default auth ((req) => {
+export default auth((req)=> {
+    const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
+
+    // if api/auth => return => true
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    // if pathname dashboard =>  publicRoutes.includes => '/dashboard ' => true
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    // "/auth/login", "/auth/register" => true
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+   if(isApiAuthRoute){
+       return null;
+   }
+
+    if(isAuthRoute){
+        // ถ้าเข้าสู่ระบบให้ไปหน้า /setting
+        if(isLoggedIn){
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+        }
+        return null;
+    }
+
+    // !logged , publie.false => /auth/login
+    if(!isLoggedIn && !isPublicRoute){
+        return Response.redirect(new URL("/auth/login", nextUrl))
+    }
+
     console.log("ROUTE: ", req.nextUrl.pathname);
     console.log("IS LOGGEDIN: ", isLoggedIn);
 })
+
 
 export const config = {
     matcher: [
